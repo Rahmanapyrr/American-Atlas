2ef8d553bd60aee3eae17e69f58ab9394902ec49
rahmanapyrr <rahmana.muhammad@bison.howard.edu>
2020-10-10 08:55:55 -0400

    logos

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { LogBox, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+        source={require('assets/LocationPin.png')}/>
+      <Text>Open up App.js to start working!!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#0f63a3',
     alignItems: 'center',
     justifyContent: 'center',
   },
